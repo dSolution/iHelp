45e61ddb5dd8bb7d658b349464f5919e0cc85597
kevinlin505 <kevinlin505@hotmail.com>
2015-10-26 13:46:51 -0400

    Adding two dummy data for testing

--- iHelp/iHelp/AppDelegate.m
+++ iHelp/iHelp/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import "IHPRequestDataStore.h"
+#import "IHPRequest.h"
+#import "IHPUser.h"
 
 @interface AppDelegate ()
 
@@ -16,112 +19,61 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+//    IHPRequestDataStore *dataStore = [IHPRequestDataStore sharedDataStore];
+//    NSManagedObjectContext *manageContext = dataStore.managedObjectContext;
+    
+//    IHPUser *ben = [NSEntityDescription insertNewObjectForEntityForName:@"IHPUser" inManagedObjectContext:manageContext];
+//    
+//    ben.username = @"leof99";
+//    ben.location = @"New York";
+//    ben.email = @"leofff123@gmail.com";
+//    ben.firstname = @"Leo";
+//    ben.lastname = @"Feng";
+//    ben.profilePicURL = @"leo";
+//    ben.gender = @"Male";
+//    ben.areaOfInterest = @"game";
+//    ben.intro = @"My name is Leo and I like to play games.";
+//    NSDate *today = [NSDate date];
+//    ben.dateJoined = [today timeIntervalSinceReferenceDate];
+//    
+//    IHPRequest *request = [NSEntityDescription insertNewObjectForEntityForName:@"IHPRequest" inManagedObjectContext:manageContext];
+//    request.requestUser = ben;
+//    request.requestTitle = @"Looking for the first edition Mario Cart game";
+//    request.requestDescription = @"Looking for the first edition Mario Cart game. Note: Need to be playable";
+//    request.requestImageURL = @"mariocart";
+//    request.requestCategory = @"game";
+//    request.requestViewCount = 100;
+//    request.requestReward = YES;
+//    request.requestRewardDescription = @"$100";
+//    request.requestDate = [today timeIntervalSinceReferenceDate];
+//    request.requestDuration = 15;
+//    request.requestStatus = @"Open";
+//    
+//    [dataStore saveContext];
+    
+//    [dataStore fetchDataWithUsername:@"leof99"];
+//    
+//    IHPUser *ben = dataStore.user;
+//    NSArray *allRequest = dataStore.requests;
+//    
+//    NSLog(@"The User is: %@", ben.firstname);
+//    NSLog(@"Below is all %@'s requests: \n", ben.firstname);
+//    for (IHPRequest *request in allRequest) {
+//        NSLog(@"Request title: %@", request.requestTitle);
+//        NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+//        dateFormat.timeStyle = NSDateFormatterNoStyle;
+//        dateFormat.dateStyle = NSDateFormatterMediumStyle;
+//        
+//        NSDate *date = [NSDate dateWithTimeIntervalSinceReferenceDate:request.requestDate];
+//        NSLocale *usLocale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+//        [dateFormat setLocale:usLocale];
+//        NSLog(@"Request Date: %@",[dateFormat stringFromDate:date]);
+//        
+//    }
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    // Saves changes in the application's managed object context before the application terminates.
-    [self saveContext];
-}
-
-#pragma mark - Core Data stack
-
-@synthesize managedObjectContext = _managedObjectContext;
-@synthesize managedObjectModel = _managedObjectModel;
-@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
-
-- (NSURL *)applicationDocumentsDirectory {
-    // The directory the application uses to store the Core Data store file. This code uses a directory named "IHP.iHelp" in the application's documents directory.
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-}
-
-- (NSManagedObjectModel *)managedObjectModel {
-    // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
-    if (_managedObjectModel != nil) {
-        return _managedObjectModel;
-    }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"iHelp" withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    return _managedObjectModel;
-}
-
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
-    // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it.
-    if (_persistentStoreCoordinator != nil) {
-        return _persistentStoreCoordinator;
-    }
-    
-    // Create the coordinator and store
-    
-    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"iHelp.sqlite"];
-    NSError *error = nil;
-    NSString *failureReason = @"There was an error creating or loading the application's saved data.";
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
-        // Report any error we got.
-        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-        dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
-        dict[NSLocalizedFailureReasonErrorKey] = failureReason;
-        dict[NSUnderlyingErrorKey] = error;
-        error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
-        // Replace this with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
-    }
-    
-    return _persistentStoreCoordinator;
-}
-
-
-- (NSManagedObjectContext *)managedObjectContext {
-    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.)
-    if (_managedObjectContext != nil) {
-        return _managedObjectContext;
-    }
-    
-    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-    if (!coordinator) {
-        return nil;
-    }
-    _managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
-    [_managedObjectContext setPersistentStoreCoordinator:coordinator];
-    return _managedObjectContext;
-}
-
-#pragma mark - Core Data Saving support
-
-- (void)saveContext {
-    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
-    if (managedObjectContext != nil) {
-        NSError *error = nil;
-        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            abort();
-        }
-    }
-}
 
 @end
