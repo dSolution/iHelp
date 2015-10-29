0f497db3ffafd928ded372df2d0ae53716158c47
wojunfeng <wjun1924@gmail.com>
2015-10-29 16:03:16 -0400

    log in partial working

--- iHelp/iHelp/IHPUserLoginViewController.m
+++ iHelp/iHelp/IHPUserLoginViewController.m
@@ -14,6 +14,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
+@property (strong, nonatomic)Firebase *rootRef;
+@property (nonatomic)BOOL isValidLogin;
+
 @end
 
 @implementation IHPUserLoginViewController
@@ -22,25 +25,59 @@
     [super viewDidLoad];
     
     // Create a reference to a Firebase database URL
-    Firebase *myRootRef = [[Firebase alloc] initWithUrl:@"https://luminous-torch-5235.firebaseIO.com"];
+    self.rootRef = [[Firebase alloc] initWithUrl:@"https://luminous-torch-5235.firebaseIO.com"];
+    
     // Write data to Firebase
-    [myRootRef setValue:@"Leo test."];
-}
-
-- (IBAction)loginButtonTapped:(id)sender {
-    if ([self isValidUsernamePassword]) {
-        [self fetchLoggedinUserData];
-    } else {
-        [self createAlert];
-    }
+    //    [myRootRef setValue:@"Leo test."];
+    
+    // Read data and react to changes
+//    [myRootRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+//        NSLog(@"%@ -> %@", snapshot.key, snapshot.value);
+//    }];
+    
+    
+//    Firebase *ref = [[Firebase alloc] initWithUrl:@"https://luminous-torch-5235.firebaseIO.com"];
+//    [ref authAnonymouslyWithCompletionBlock:^(NSError *error, FAuthData *authData) {
+//        if (error) {
+//            // There was an error logging in anonymously
+//        } else {
+//            // We are now logged in
+//            NSLog(@"logged in anonymously");
+//        }
+//    }];
+    
+    
+    //create firebase new user
+//    [myRootRef createUser:@"leof999@test.com" password:@"123456" withValueCompletionBlock:^(NSError *error, NSDictionary *result) {
+//        if (error) {
+//            // There was an error creating the account
+//        } else {
+//            NSString *uid = [result objectForKey:@"uid"];
+//            NSLog(@"Successfully created user account with uid: %@", uid);
+//        }
+//    }];
+//    
     
 }
 
+- (IBAction)loginButtonTapped:(id)sender {
+    //log in with User credential
+    [self.rootRef authUser:self.usernameField.text password:self.passwordField.text withCompletionBlock:^(NSError *error, FAuthData *authData) {
+        if (error) {
+            // There was an error logging in to this account
+            self.isValidLogin = NO;
+            [self createAlert];
+
+            
+        } else {
+            self.isValidLogin = YES;
+            [self fetchLoggedinUserData];
+            NSLog(@"logged in successfully");
+        }
+    }];
+}
+
 -(BOOL)isValidUsernamePassword{
-//    IHPRequestDataStore *data = [IHPRequestDataStore sharedDataStore];
-//    if (data.user) {
-//        return YES;
-//    }
     return YES;
 }
 
@@ -60,7 +97,11 @@
 - (void)fetchLoggedinUserData{
     IHPRequestDataStore *data = [IHPRequestDataStore sharedDataStore];
     [data fetchDataWithUsername:@"leof99"];
-
 }
+
+- (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender{
+    return self.isValidLogin;
+}
+
 
 @end
