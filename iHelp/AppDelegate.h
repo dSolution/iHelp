cc6a35891dca3e51cd24fd2f2f8cfaf4305f8943
kevinlin505 <kevinlin505@hotmail.com>
2015-11-09 09:29:12 -0500

    Initial Commit

---
+++ iHelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iHelp
+//
+//  Created by Kevin Lin on 11/9/15.
+//  Copyright © 2015 dSolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iHelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iHelp
+//
+//  Created by Kevin Lin on 11/9/15.
+//  Copyright © 2015 dSolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
